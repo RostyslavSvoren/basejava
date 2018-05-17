f9804656847ff07f93c44fdd2e8a76dc4682fe79
Rostyslav <rostyslav.svoren@gmail.com>
2018-05-17 22:04:47 +0300

    HW2 Refactor code.

--- src/MainArray.java
+++ src/MainArray.java
@@ -58,7 +58,7 @@ public class MainArray {
         }
     }
 
-   private static void printAll() {
+    private static void printAll() {
         Resume[] all = ARRAY_STORAGE.getAll();
         System.out.println("----------------------------");
         if (all.length == 0) {
